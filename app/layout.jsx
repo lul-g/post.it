599f6a9ebbdce1a21bd83870bad7f38461f52e92
Lulseged Admasu <lulsegedgirma10@gmail.com>
2023-07-28 03:00:21 -0400

    adding html file for google to verify the site

--- app/layout.jsx
+++ app/layout.jsx
@@ -21,6 +21,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="RuuXp2wp67anc2vahqF_-sZl2Md89Hq7GxYL8dsBUKE"
+        />
+      </Head>
       <body className={`${roboto.variable} font-roboto bg-black`}>
         <Providers>
           <div className="mx-auto" style={{ maxWidth: "500px" }}>
